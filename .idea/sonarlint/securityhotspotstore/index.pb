3527d61ce3676ccb124d0f9cf6f056c0b98bb2eb
Prateek Giri <prateek.giri@conceptext.com>
2024-04-30 14:38:33 +0530

    gfg_potd_add_two_number

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+Q
+!src/LinkeList/add_two_number.java,c\7\c792f9c3964e39da7e99d91cfaa4e5ccb52db3cf
+R
+"src/LinkedList/add_two_number.java,e\2\e298a841a0ca7213dfc7a24103dc293a29432f2f
---
+++ src/LinkedList/add_two_number.java
@@ -0,0 +1,110 @@
+package LinkedList;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class add_two_number {
+    static Node addTwoLists(Node num1, Node num2){
+        // code here
+        // return head of sum list
+        Node n1 = reverseList(num1);
+        Node n2 = reverseList(num2);
+
+        Node dummy = new Node(0);
+        Node curr = dummy;
+
+        int carry = 0;
+        while(n1 != null &&  n2 != null){
+            int val1 = n1.data;
+            int val2 = n2.data;
+            int val_sum = val1 + val2 + carry;
+            if(val_sum > 9)
+            {
+                val_sum = val_sum % 10;
+                carry = 1;
+            }
+            else
+            {
+                carry = 0;
+            }
+            Node node = new Node(val_sum);
+            curr.next = node;
+            curr = curr.next;
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        while(n1 != null){
+            int val1 = n1.data;
+            int val_sum = val1 + carry;
+            if(val_sum > 9)
+            {
+                val_sum = val_sum % 10;
+                carry = 1;
+            }
+            else
+            {
+                carry = 0;
+            }
+            Node node = new Node(val_sum);
+            curr.next = node;
+            curr = curr.next;
+            n1 = n1.next;
+        }
+        while(n2 != null){
+            int val2 = n2.data;
+            int val_sum = val2 + carry;
+            if(val_sum > 9)
+            {
+                val_sum = val_sum % 10;
+                carry = 1;
+            }
+            else
+            {
+                carry = 0;
+            }
+            Node node = new Node(val_sum);
+            curr.next = node;
+            curr = curr.next;
+            n2 = n2.next;
+        }
+        if(carry > 0){
+            Node node = new Node(carry);
+            curr.next = node;
+        }
+        Node y = reverseList(dummy.next);
+        while(y != null)
+        {
+            if(y.data == 0)
+            {
+                y = y.next;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return y == null ? new Node(0) :  y;
+
+    }
+    public static Node reverseList(Node head) {
+        Node prev = null;
+        Node curr = head;
+        Node next = head;
+
+        while(next != null){
+            next = next.next;
+            curr.next = prev;
+            prev = curr;
+            if(next != null) curr = next;
+        }
+        return curr;
+    }
+
+}
